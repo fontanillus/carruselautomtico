711edde54093456e1500845d7634e5a0e8c46d6b
fontanillus <arcamapa@gmail.com>
2025-07-22 12:00:27 +0200

    Add files via upload

---
+++ js/carrusell.js
@@ -0,0 +1,27 @@
+const RUTA = "./img/";
+const imagenes = [
+    {
+        archivo: "barcelona.jpg",
+        nombre: "Barcelona",
+        tooltip: "Ciudad de España",
+        descripcion: "Capital de Catalunya, cuna de arquitectura"
+    },
+    {
+        archivo: "newyork.jpg",
+        nombre: "New York",
+        tooltip: "Capital finaciera de Estados Unidos",
+        descripcion: "New York, la gran manzana"
+    },
+    {
+        archivo: "paris.jpg",
+        nombre: "París",
+        tooltip: "Capital de Francia",
+        descripcion: "París, ciudad de la luz"
+    },
+    {
+        archivo: "londres.jpg",
+        nombre: "Londres",
+        tooltip: "Capital de Inglaterra",
+        descripcion: "Ciudad jardín. Preciosa, Cosmopolita... cuna de los Beatles"
+    }
+];
